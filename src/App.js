d8842982cd4e4b5fc828ef46dfd8f54fe1c0d0f8
Sushruth Chandrasekar <sushruth31@gmail.com>
2022-06-12 17:04:16 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Grid from "./grid"
-import { useState } from "react"
+import { useMemo, useState } from "react"
+import bombImgSrc from "./bomb.png"
 
 const NUM_ROWS = 16
 const NUM_COLS = 16
 
-function toKey(arr) {
+export function toKey(arr) {
   return arr[0] + "-" + arr[1]
 }
 
@@ -14,21 +15,45 @@ let colorMap = {
   3: "red",
 }
 
+function plantBombs() {
+  let numBombs = Math.floor(Math.sqrt(NUM_ROWS * NUM_COLS))
+  let bombs = new Set()
+  let map = new Map()
+
+  function createBombKey() {
+    let randRow = Math.floor(Math.random() * NUM_ROWS)
+    let randCol = Math.floor(Math.random() * NUM_COLS)
+    let bombAttempt = toKey([randRow, randCol])
+    if (bombs.has(bombAttempt)) {
+      return createBombKey()
+    }
+    bombs.add(bombAttempt)
+    return bombAttempt
+  }
+
+  for (let i = 0; i < numBombs; i++) {
+    map.set(createBombKey(), "bomb")
+  }
+  return map
+}
+
+function getNeihbors(key) {}
+
+function createMap() {
+  //loop through each sq in grid. get neihbors. count number of bombs. combine maps
+  let bombMap = plantBombs()
+  return bombMap
+}
+
 export default function App() {
   let [revealed, setRevealed] = useState(new Set())
-  let [map, setMap] = useState(
-    new Map([
-      ["1-1", 1],
-      ["1-2", 1],
-      ["1-3", null],
-    ])
-  )
+  let map = useMemo(createMap, [])
 
-  function renderCell({ rowI, colI }) {
-    let val = map.get(toKey([rowI, colI]))
+  function renderCell({ cellKey }) {
+    let val = map.get(cellKey)
     return (
       <div className="font-bold text-2xl" style={{ color: colorMap[val] }}>
-        {val}
+        {val === "bomb" ? <img className="p-1" src={bombImgSrc} /> : val}
       </div>
     )
   }
@@ -36,7 +61,17 @@ export default function App() {
   return (
     <div className="flex items-center flex-col p-4">
       <h1 className="text-2xl font-bold mb-10">Minesweeper</h1>
-      <Grid numCols={NUM_COLS} numRows={NUM_ROWS} renderCell={renderCell} />
+      <Grid
+        onCellClick={key => {
+          if (revealed.has(key)) {
+            return
+          }
+          setRevealed(p => new Set([...p, key]))
+        }}
+        numCols={NUM_COLS}
+        numRows={NUM_ROWS}
+        renderCell={renderCell}
+      />
     </div>
   )
 }
--- src/grid.js
+++ src/grid.js
@@ -1,16 +1,25 @@
-export default function Grid({ numRows, numCols, renderCell: RenderCell }) {
+import { toKey } from "./App"
+
+export default function Grid({
+  onCellClick,
+  numRows,
+  numCols,
+  renderCell: RenderCell,
+}) {
   return (
     <>
       {Array.from(Array(numRows)).map((_, rowI) => {
         return (
           <div className="flex border border-zinc-500" key={rowI}>
             {Array.from(Array(numCols)).map((_, colI) => {
+              let cellKey = toKey([rowI, colI])
               return (
                 <div
+                  onClick={() => onCellClick(cellKey)}
                   className="w-[40px] cursor-pointer flex items-center justify-center h-[40px] border border-zinc-500"
                   key={colI}
                 >
-                  <RenderCell colI={colI} rowI={rowI} />
+                  <RenderCell cellKey={cellKey} colI={colI} rowI={rowI} />
                 </div>
               )
             })}
