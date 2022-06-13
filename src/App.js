e8165e7b51e707cc232eeed11586f67c36b7d23f
Sushruth Chandrasekar <sushruth31@gmail.com>
2022-06-12 20:19:33 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ export function toKey(arr) {
   return arr[0] + "-" + arr[1]
 }
 
+function toArr(key) {
+  return key.split("-").map(Number)
+}
+
 let colorMap = {
   1: "blue",
   2: "green",
@@ -37,17 +41,57 @@ function plantBombs() {
   return map
 }
 
-function getNeihbors(key) {}
+function filterInvalid([r, c]) {
+  return r >= 0 && r <= NUM_ROWS && c >= 0 && c <= NUM_COLS
+}
+window.getNeighbors = getNeighbors
+
+//['1-1', '2-2']
+function getNeighbors(key) {
+  let [r, c] = toArr(key)
+  let neihbors = [
+    [r - 1, c - 1],
+    [r - 1, c],
+    [r - 1, c + 1],
+    [r, c - 1],
+    [r, c + 1],
+    [r + 1, c - 1],
+    [r + 1, c],
+    [r + 1, c + 1],
+  ]
+    .filter(filterInvalid)
+    .map(toKey)
+  return neihbors
+}
 
 function createMap() {
-  //loop through each sq in grid. get neihbors. count number of bombs. combine maps
   let bombMap = plantBombs()
+  //loop through each sq in grid. get neihbors. count number of bombs. combine maps
+  for (let i = 0; i < NUM_ROWS; i++) {
+    for (let j = 0; j < NUM_COLS; j++) {
+      let key = toKey([i, j])
+      //if it is a bomb go to next square
+      if (bombMap.get(key)) {
+        continue
+      }
+      let neighbors = getNeighbors(key)
+      let count = neighbors.reduce((acc, cur) => {
+        if (bombMap.get(cur) === "bomb") {
+          return acc + 1
+        }
+        return acc
+      }, 0)
+      bombMap.set(key, count <= 3 ? count : null)
+    }
+  }
+
   return bombMap
 }
 
 export default function App() {
   let [revealed, setRevealed] = useState(new Set())
   let map = useMemo(createMap, [])
+  window.bombMap = map
 
   function renderCell({ cellKey }) {
     let val = map.get(cellKey)
